e524ea66f9fd48cc630dc76b6cd6c1f57395198a
sidewinder2020 <sdwinder1@gmail.com>
2017-11-01 10:45:59 -0600

    finished store poro, working on figaro for the api service

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,11 +2,11 @@ class Store
   attr_reader :long_name, :city, :distance, :phone_number, :store_type
 
     def initialize(attrs = {})
-      @long_name = attrs[:]
-      @city = attrs[:]
-      @distance = attrs[:]
-      @phone_number = attrs[:]
-      @store_type = attrs[:]
+      @long_name = attrs[:longName]
+      @city = attrs[:city]
+      @distance = attrs[:distance]
+      @phone_number = attrs[:phone]
+      @store_type = attrs[:storeType]
     end
 
     #long name, city, distance, phone number and store type
--- app/services/best_buy_api_service.rb
+++ app/services/best_buy_api_service.rb
@@ -1,7 +1,5 @@
 class BestBuyApiService
 
-  #https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&pageSize=10&apiKey=6tmxnbn286qhnt4act7h3tt8
-
   def initialize
     @conn = Faraday.new('https://api.bestbuy.com/v1/') do |faraday|
       faraday.headers["X-API-KEY"] = "ENV['best_buy_api_key']"
@@ -10,8 +8,8 @@ class BestBuyApiService
   end
 
   def find_by_zipcode(zipcode)
-    response = @conn.get("stores(area(#{zipcode},25))?format=json&pageSize=10&apiKey=6tmxnbn286qhnt4act7h3tt8")
-    JSON.parse(response.body, symbolize_names: true)
+    response = @conn.get("stores(area(#{zipcode},25))?format=json&pageSize=10&apiKey=#{ENV['best_buy_api_key']}")
+    JSON.parse(response.body, symbolize_names: true)[:stores]
   end
 
   private
