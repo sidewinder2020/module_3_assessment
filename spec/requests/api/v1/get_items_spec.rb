69442d248fe42eb228c46dbe00be1f13223ceb80
sidewinder2020 <sdwinder1@gmail.com>
2017-11-01 09:29:20 -0600

    Created tests for api/v1/items build api portion

--- spec/requests/api/v1/get_items_spec.rb
+++ spec/requests/api/v1/get_items_spec.rb
@@ -1,77 +1,62 @@
 require 'rails_helper'
 
-describe "Get /api/v1/destinations/:id" do
-  it "returns a destination" do
-    destination = Destination.create!(name: "Green Cove Springs", zip: "32043", description: "a place where dreams go to die", image_url: "https://placehold.it/300x300.png/000")
+describe "Get /api/v1/items/:id" do
+  it "returns an item" do
+    item = Item.create!(name: "Babbadook", description: "a scary movie", image_url: "https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
 
-    get "/api/v1/destinations/#{destination.id}"
+    get "/api/v1/items/#{item.id}"
 
     response = JSON.parse(body)
 
     expect(response["id"]).to eq(1)
-    expect(response["name"]).to eq("Green Cove Springs")
-    expect(response["description"]).to eq("a place where dreams go to die")
-    expect(response["zip"]).to eq("32043")
+    expect(response["name"]).to eq("Babbadook")
+    expect(response["description"]).to eq("a scary movie")
+    expect(response["image_url"]).to eq("https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
   end
 end
 
-describe "Get /api/v1/destinations" do
-  it "returns all destinations" do
-    destination = Destination.create!(name: "Green Cove Springs", zip: "32043", description: "a place where dreams go to die", image_url: "https://placehold.it/300x300.png/000")
-    destination_2 = Destination.create!(name: "Orange Park", zip: "32073", description: "hellscape", image_url: "https://placehold.it/300x300.png/000")
+describe "Get /api/v1/items" do
+  it "returns all items" do
+    item = Item.create!(name: "Babbadook", description: "a scary movie", image_url: "https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
+    item_2 = Item.create!(name: "Furry Costume", description: "costume for fun, furry conventions", image_url: "https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
 
-    get "/api/v1/destinations"
+    get "/api/v1/items"
 
     response = JSON.parse(body)
 
     expect(response.count).to eq 2
-    expect(response[0]["id"]).to eq 1
-    expect(response[0]["name"]).to eq("Green Cove Springs")
-    expect(response[0]["description"]).to eq("a place where dreams go to die")
-    expect(response[0]["zip"]).to eq("32043")
+    expect(response["id"]).to eq(1)
+    expect(response["name"]).to eq("Babbadook")
+    expect(response["description"]).to eq("a scary movie")
+    expect(response["image_url"]).to eq("https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
   end
 end
 
-describe "Post /api/v1/destinations" do
-  it "creates a destination" do
-    post "/api/v1/destinations?destination[name]=Hello&destination[zip]=33333&destination[description]=hell&destination[image_url]=https://placehold.it/300x300.png/000"
+describe "Post /api/v1/items" do
+  it "creates an item" do
+    post "/api/v1/destinations?item[name]=Crazy&item[description]=hell&item[image_url]=https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg"
 
     response = JSON.parse(body)
 
     expect(response["id"]).to eq(1)
-    expect(response["name"]).to eq("Hello")
+    expect(response["name"]).to eq("Crazy")
     expect(response["description"]).to eq("hell")
-    expect(response["zip"]).to eq("33333")
+    expect(response["image_url"]).to eq("https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
   end
 end
 
-describe "PUT /api/v1/destinations/:id" do
-  it "updates a destination" do
-    destination = Destination.create!(name: "Green Cove Springs", zip: "32043", description: "a place where dreams go to die", image_url: "https://placehold.it/300x300.png/000")
+describe "DELETE /api/v1/items/:id" do
+  it "deletes an item" do
+    item = Item.create!(name: "Babbadook", description: "a scary movie", image_url: "https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
 
-    put "/api/v1/destinations/#{destination.id}?destination[name]=Orange%20Park"
+    delete "/api/v1/items/#{item.id}"
 
     response = JSON.parse(body)
 
     expect(response["id"]).to eq(1)
-    expect(response["name"]).to eq("Orange Park")
-    expect(response["description"]).to eq("a place where dreams go to die")
-    expect(response["zip"]).to eq("32043")
-  end
-end
-
-describe "DELETE /api/v1/destinations/:id" do
-  it "deletes a destination" do
-    destination = Destination.create!(name: "Green Cove Springs", zip: "32043", description: "a place where dreams go to die", image_url: "https://placehold.it/300x300.png/000")
-
-    delete "/api/v1/destinations/#{destination.id}"
-
-    response = JSON.parse(body)
-
-    expect(response["id"]).to eq(1)
-    expect(response["name"]).to eq("Green Cove Springs")
-    expect(response["description"]).to eq("a place where dreams go to die")
-    expect(response["zip"]).to eq("32043")
-    expect(Destination.count).to eq 0
+    expect(response["name"]).to eq("Babbadook")
+    expect(response["description"]).to eq("a scary movie")
+    expect(response["image_url"]).to eq("https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg")
+    expect(Item.count).to eq 0
   end
 end
