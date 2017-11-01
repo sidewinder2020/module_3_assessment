34a714b35ee9eb34813ed1afdf0d1b72cddeee0a
sidewinder2020 <sdwinder1@gmail.com>
2017-11-01 09:19:19 -0600

    api/v1/items controller and routes up and running

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: Item.all
+  end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+      if @item.save
+        render json: @item
+      else
+        render :status => 404
+      end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      render json: @item
+    else
+      render :status => 404
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
+  end
+end
