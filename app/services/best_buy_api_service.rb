d906821590fc8dd22c0d9425df507ace74b83d5c
sidewinder2020 <sdwinder1@gmail.com>
2017-11-01 10:34:39 -0600

    api service has a working full address - trying to split it up

--- app/services/best_buy_api_service.rb
+++ app/services/best_buy_api_service.rb
@@ -1,13 +1,16 @@
 class BestBuyApiService
+
+  #https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&pageSize=10&apiKey=6tmxnbn286qhnt4act7h3tt8
+
   def initialize
-    @conn = Faraday.new('') do |faraday|
+    @conn = Faraday.new('https://api.bestbuy.com/v1/') do |faraday|
       faraday.headers["X-API-KEY"] = "ENV['best_buy_api_key']"
       faraday.adapter Faraday.default_adapter
     end
   end
 
   def find_by_zipcode(zipcode)
-    response = @conn.get("#{zipcode}")
+    response = @conn.get("stores(area(#{zipcode},25))?format=json&pageSize=10&apiKey=6tmxnbn286qhnt4act7h3tt8")
     JSON.parse(response.body, symbolize_names: true)
   end
 
